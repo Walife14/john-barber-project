6a7c058b02da5218ed9c80d1d8a840ebca173f66
Walife14 <lferon14@gmail.com>
2022-03-30 23:40:15 +0100

    nav tweak
    + navbar position now adjusted using JavaScript, toggling between absolute and fixed position.

--- app.js
+++ app.js
@@ -2,17 +2,29 @@
 // nav toggle
 
 const hamburger = document.querySelector('.hamburger')
+const navElement = document.querySelector('nav')
 const navbar = document.querySelector('.navbar')
+const navLinks = document.querySelectorAll('.nav-link')
 
 const toggleNav = () => {
     if (hamburger.classList.contains('navbar-x')) {
         hamburger.classList.remove('navbar-x')
         navbar.classList.remove('show')
+        navElement.style.position = 'absolute'
     }
     else {
         navbar.classList.add('show')
         hamburger.classList.add('navbar-x')
+        navElement.style.position = 'fixed'
     }
 }
+
+console.log(navElement)
+
+// navLinks.forEach(e => {
+//     e.
+// })
+
+// console.log(navLinks)
 
 hamburger.addEventListener('click', toggleNav)
